94a26feaac9f13b528964f90d329f0b0ced1ccaf
redbau <wildercraftserver@gmail.com>
2021-09-05 03:09:13 -0400

    Remove update check so plugin starts up

---
+++ changelog.md
@@ -0,0 +1,2 @@
+2.0
+- [fix] Remove auto updater check, add spigot 1.17
--- src/com/widgetpowered/watchdog/WatchDog.java
+++ src/com/widgetpowered/watchdog/WatchDog.java
@@ -31,7 +31,7 @@ public class WatchDog extends JavaPlugin {
 	 */
 	@Override
 	public void onEnable() {
-		new UpdateCheck(this).checkForUpdates();
+//		new UpdateCheck(this).checkForUpdates();
 		
 		saveDefaultConfig();
 		
